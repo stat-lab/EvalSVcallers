7a617908dcce77834d65fd74a3537615686e774c
stat-lab <13638380+stat-lab@users.noreply.github.com>
2020-08-05 11:33:52 +0900

    Add files via upload

---
+++ scripts/vcf_convert/convert_Vaquita_vcf.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl -w
+use strict;
+
+# covert Vaquita output files to vcf
+
+my $min_sv_len = 30;
+
+my $min_reads = 2;
+
+my %vcf;
+my $count = 0;
+
+foreach my $file (@ARGV){
+    if (!-e $file){
+	print STDERR "$file is not found\n";
+	next;
+    }
+    $count ++;
+    open (FILE, $file);
+    while (my $line = <FILE>){
+	chomp $line;
+	if ($line =~ /^#/){
+	    next;
+	}
+	my @line = split (/\t/, $line);
+    if (@line >= 11){
+        if ($line[9] =~ /:0:0:0$/){
+            next;
+        }
+    }
+	my $chr = $line[0];
+	my $pos = $line[1];
+	my $type = $1 if ($line[7] =~ /SVTYPE=(.+)/);
+    	$type = substr $type, 0, 3;
+	next if ($type eq 'BND');
+	my $len = 0;
+	$len = $1 if ($line[7] =~ /SVLEN=-*(\d+);/);
+	next if ($len < $min_sv_len) and ($len > 0);
+	my $reads = 0;
+	$reads = $1 if ($line[7] =~ /SE=(\d+);/);
+	$reads += $1 if ($line[7] =~ /PE=(\d+);/);
+	$reads += $1 if ($line[7] =~ /CE=(\d+);/);
+	my $gt = '';
+	next if ($chr !~ /^\d+$|[XY]/);
+	print "$chr\t$pos\t$type\t.\t.\t.\tPASS\tSVTYPE=$type;SVLEN=$len;READS=$reads\n";
+    }
+    close (FILE);
+}
