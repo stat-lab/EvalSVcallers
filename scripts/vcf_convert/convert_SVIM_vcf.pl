8b66957111d42b409e8fca2b669509c73a17ae0a
stat-lab <13638380+stat-lab@users.noreply.github.com>
2022-01-02 21:13:52 +0900

    Update convert_SVIM_vcf.pl

--- scripts/vcf_convert/convert_SVIM_vcf.pl
+++ scripts/vcf_convert/convert_SVIM_vcf.pl
@@ -1,12 +1,7 @@
 #!/usr/bin/perl -w
 use strict;
 
-# covert SVIM output vcf file to vcf
-
 my $file = shift @ARGV;
-
-my $min_size = 50;
-my $min_read = 2;
 
 open (FILE, $file) or die "$file is not found: $!\n";
 while (my $line = <FILE>){
@@ -14,7 +9,6 @@ while (my $line = <FILE>){
     next if ($line =~ /^#/);
     my @line = split (/\t/, $line);
     my $chr = $line[0];
-    next if ($chr !~ /^[\dXY]+/);
     my $pos = $line[1];
     my $type = $1 if ($line[7] =~ /SVTYPE=(.+?)[:;]/);
     next if ($type !~ /^DEL|^INS|^INV|^DUP/);
@@ -25,9 +19,7 @@ while (my $line = <FILE>){
         $end = $1 if ($line[7] =~ /END=(\d+)/);
         $len = $end - $pos + 1;
     }
-    next if ($len < $min_size);
     my $reads = $1 if ($line[7] =~ /SUPPORT=(\d+)/);
-    next if ($reads < $min_read);
     print "$chr\t$pos\t$type\t.\t.\t.\tPASS\tSVTYPE=$type;SVLEN=$len;READS=$reads\n";
 }
 close (FILE);
