003a9d6e48532d5d031f500e5a535d32dcc12d7e
stat-lab <13638380+stat-lab@users.noreply.github.com>
2022-01-02 21:15:38 +0900

    Update convert_SVfinder_vcf.pl

--- scripts/vcf_convert/convert_SVfinder_vcf.pl
+++ scripts/vcf_convert/convert_SVfinder_vcf.pl
@@ -1,10 +1,6 @@
 #!/usr/bin/perl -w
 use strict;
 
-# covert Delly output files to vcf
-
-my $min_sv_len = 30;
-
 my %var;
 
 foreach my $var_file (@ARGV){
@@ -12,7 +8,7 @@ foreach my $var_file (@ARGV){
     while (my $line = <FILE>){
         chomp $line;
         if ($line =~ /^type/){
-        next;
+            next;
         }
         my @line = split (/\t/, $line);
         my $chr = $line[2];
@@ -29,43 +25,43 @@ foreach my $var_file (@ARGV){
         my $chr2 = '';
         my $len = 0;
         if ($type eq 'Inversion'){
-        $type = 'INV';
+            $type = 'INV';
         }
         elsif ($type eq 'Deletion'){
-        $type = 'DEL';
+            $type = 'DEL';
         }
         elsif ($type eq 'Insertion'){
-        $type = 'INS';
+            $type = 'INS';
         }
         elsif ($type eq 'Intrachromosomal_translocation'){
-        $chr2 = $line[7];
-        if ($chr eq $chr2){
-            $type = 'INS-T';
+            $chr2 = $line[7];
+            if ($chr eq $chr2){
+                $type = 'INS-T';
+            }
+            else{
+                $type = 'TRA';
+            }
         }
-        else{
-            $type = 'TRA';
-        }
-        }
-        elsif ($type eq 'Interchromosomal_translocation'){
-        $chr2 = $line[7];
-        $type = 'TRA';
-        }
-        if (($bp1 > $bp2) and ($type ne 'INS-T')){
-        $pos = $bp2;
-        $end = $bp1;
-        $bp1_s = $line[7];
-        $bp1_e = $line[8];
-        $bp2_s = $line[3];
-        $bp2_e = $line[4];
-        }
-        
-        $len = $end - $pos + 1 if ($type ne 'INS-T');
-        $type = 'INS' if ($type eq 'INS-T');
-        my $pos2 = 0;
-        if ($type eq 'TRA'){
-        $end = 0;
-        $len = 0;
-        }
+            elsif ($type eq 'Interchromosomal_translocation'){
+                $chr2 = $line[7];
+                $type = 'TRA';
+            }
+            if (($bp1 > $bp2) and ($type ne 'INS-T')){
+                $pos = $bp2;
+                $end = $bp1;
+                $bp1_s = $line[7];
+                $bp1_e = $line[8];
+                $bp2_s = $line[3];
+                $bp2_e = $line[4];
+            }
+
+            $len = $end - $pos + 1 if ($type ne 'INS-T');
+            $type = 'INS' if ($type eq 'INS-T');
+            my $pos2 = 0;
+            if ($type eq 'TRA'){
+                $end = 0;
+                $len = 0;
+            }
         my $reads = $line[1];
         ${${$var{$chr}}{$pos}}{$type} = "$chr\t$pos\t$type\t.\t.\t.\tPASS\tSVTYPE=$type;SVLEN=$len;READS=$reads" if ($type ne 'TRA');
         ${${$var{$chr}}{$pos}}{$type} = "$chr\t$pos\t$type\t.\t.\t.\tPASS\tSVTYPE=$type;SVLEN=$len;READS=$reads\tCHR2=$chr2;POS2=$pos2" if ($type eq 'TRA');
