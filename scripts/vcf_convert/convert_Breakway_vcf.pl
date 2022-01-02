9afe9be4fbe692a104475b52f30aeb2ade8f0b45
stat-lab <13638380+stat-lab@users.noreply.github.com>
2022-01-02 20:24:53 +0900

    Update convert_Breakway_vcf.pl

--- scripts/vcf_convert/convert_Breakway_vcf.pl
+++ scripts/vcf_convert/convert_Breakway_vcf.pl
@@ -1,9 +1,6 @@
 #!/usr/bin/perl -w
 use strict;
 
-
-my $min_sv_len = 30;
-
 my %vcf;
 
 foreach my $var_file (@ARGV){
@@ -11,7 +8,7 @@ foreach my $var_file (@ARGV){
     while (my $line = <FILE>){
         chomp $line;
         if ($line =~ /^chrom1:/){
-        next;
+            next;
         }	
         my @line = split (/\t/, $line);
         my $chr = '';
@@ -22,27 +19,26 @@ foreach my $var_file (@ARGV){
         my $bp2_s = 0;
         my $bp2_e = 0;
         if ($line[0] =~ /(.+?):(\d+)-(\d+)/){
-        $chr = $1;
-        $bp1_s = $2;
-        $bp1_e = $3;
-        $pos = int (($bp1_s + $bp1_e) / 2);
+            $chr = $1;
+            $bp1_s = $2;
+            $bp1_e = $3;
+            $pos = int (($bp1_s + $bp1_e) / 2);
         }
         if ($line[1] =~ /(.+?):(\d+)-(\d+)/){
-        $bp2_s = $2;
-        $bp2_e = $3;
-        $end = int (($bp2_s + $bp2_e) / 2);
+            $bp2_s = $2;
+            $bp2_e = $3;
+            $end = int (($bp2_s + $bp2_e) / 2);
         }
         if ($pos > $end){
-        my $end2 = $end;
-        $end = $pos;
-        $pos = $end2;
+            my $end2 = $end;
+            $end = $pos;
+            $pos = $end2;
         }
         next if ($chr !~ /^[\dXY]+$/);
         my $type = $line[2];
         $pos = int (($end + $pos) / 2) if ($type eq 'INS');
         my $len = 0;
         $len = $end - $pos + 1 if ($type ne 'INS');
-        next if ($len < $min_sv_len) and ($type ne 'INS');
         my $reads = $line[3];
         my $chr02d = $chr;
         $chr02d = sprintf ("%02d", $chr) if ($chr =~ /^\d+$/);
