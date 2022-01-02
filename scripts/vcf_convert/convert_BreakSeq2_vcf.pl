aa873b65fd2e93f63b545716ee16b5156744fd6b
stat-lab <13638380+stat-lab@users.noreply.github.com>
2022-01-02 20:23:36 +0900

    Update convert_BreakSeq2_vcf.pl

--- scripts/vcf_convert/convert_BreakSeq2_vcf.pl
+++ scripts/vcf_convert/convert_BreakSeq2_vcf.pl
@@ -5,8 +5,6 @@ use strict;
 
 my $var_file = shift @ARGV;
 
-my $min_sv_len = 30;
-
 my %vcf;
 
 open (FILE, $var_file) or die "$var_file is not found: $!\n";
@@ -22,14 +20,13 @@ while (my $line = <FILE>){
     my $type = $1 if ($line[7] =~ /SVTYPE=(.+?);/);
     my $len = 0;
     $len = $1 if ($line[7] =~ /SVLEN=-*(\d+);/);
-    next if ($len < $min_sv_len) and ($len > 0);
     my $end = $pos + $len - 1;
     my $qual = $line[6];
     my $reads = 7;
     next if ($qual ne 'PASS');
     my $chr_02d = $chr;
     $chr_02d = sprintf ("%02d", $chr) if ($chr =~ /^\d+$/);
-    next if ($chr !~ /^chr/) and ($chr !~ /^\d+$|[XY]/);
+    next if ($chr !~ /^c*h*r*[\dXY]+$/);
     my $gt = './.';
     $gt = $1 if ($line[-1] =~ /([^:]+)/);
     print "$chr\t$pos\t$type\t.\t.\t.\t$qual\tSVTYPE=$type;SVLEN=$len;READS=$reads;GT=$gt\n";
